da6057d5132cb4ee8ff2ee67e5bd6ace95af3e53
Laryssa Gabriela <laryssabernardo11@gmail.com>
2020-11-12 20:03:01 -0300

    adição av5

--- back-end/classe.py
+++ back-end/classe.py
@@ -1,22 +1,5 @@
 from config import *
 
-class Ranking(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    posicao = db.Column(db.Integer)
-    autor = db.Column(db.String(254))
-
-    def __str__(self):
-        return str(self.id)+", " + str(self.posicao) + ", " +\
-            self.autor
-
-    def json(self):
-        return ({
-            "id":self.id,
-            "posicao":self.posicao,
-            "autor": self.autor,
-        })
-
 class Comida(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -25,52 +8,63 @@ class Comida(db.Model):
     origem = db.Column(db.String(254))
     dificuldade_de_preparo = db.Column(db.String(254))
     nota = db.Column(db.Integer)
-    ranking_id = db.Column(db.Integer, db.ForeignKey(Ranking.id))
-    ranking = db.relationship("Ranking")
 
     def __str__(self):
-        s = f"Comida {self.nome}"
-        if self.ranking_id != None:
-            s += f"na posição {self.posicao} feito(a) por {self.autor}" 
-            return s
+        return self.sabor + ", " + self.origem + ", " +\
+            self.dificuldade_de_preparo + str(self.nota)
 
     def json(self):
-        if self.ranking_id is None:
-            return ({
-                "id": self.id,
-                "nome": self.nome,
-                "sabor": self.sabor,
-                "origem": self.origem,
-                "dificuldade_de_preparo": self.dificuldade_de_preparo,
-                "nota": self.nota
-        })
-        else:
-            return ({
-                "id": self.id,
-                "nome": self.nome,
-                "sabor": self.sabor,
-                "origem": self.origem,
-                "dificuldade_de_preparo": self.dificuldade_de_preparo,
-                "nota": self.nota,
-                "ranking": self.ranking.json()
-            })
+        return {
+            "id": self.id,
+            "nome": self.nome,
+            "sabor": self.sabor,
+            "origem": self.origem,
+            "dificuldade_de_preparo": self.dificuldade_de_preparo,
+            "nota": self.nota,
+        }
 
 class Classificacao(db.Model):
 
     id = db.Column(db.Integer, primary_key=True) 
     titulo = db.Column(db.String(254)) 
     categoria = db.Column(db.String(254))
+    data_classificacao = db.Column(db.String(254))
     comida_id = db.Column(db.Integer, db.ForeignKey(Comida.id), nullable=False)
     comida = db.relationship("Comida")
 
     def __str__(self): 
-        return str(self.id)+", " + self.titulo + ", " +\
-            self.categoria
+        return self.titulo + ", " + self.data_classificacao +\
+            self.categoria + str(self.comida)
 
     def json(self):
         return ({
             "id": self.id,
             "titulo": self.titulo,
+            "data_classificacao": self.data_classificacao,
             "categoria": self.categoria,
+            "comida_id": self.comida_id,
             "comida": self.comida.json()
         }) 
+
+class Ranking(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    posicao = db.Column(db.Integer)
+    autor = db.Column(db.String(254))
+    comida_id = db.Column(db.Integer, db.ForeignKey(Comida.id))
+    comida = db.relationship("Comida")
+
+    def __str__(self):
+        return str(self.posicao) + ", " +\
+            self.autor + str(self.comida)
+
+    def json(self):
+        return ({
+            "id":self.id,
+            "posicao":self.posicao,
+            "autor": self.autor,
+            "comida_id": self.comida_id,
+            "comida": self.comida.json()
+        })
+
+
--- back-end/teste.py
+++ back-end/teste.py
@@ -8,68 +8,55 @@ if __name__ == "__main__":
 
     db.create_all()
 
-    r1 = Ranking(posicao = 1, autor = "Massimo Bottura")
-    r2 = Ranking(posicao = 2, autor = "Enrique Olvera")
-    r3 = Ranking(posicao = 3, autor = "Jody Adams")
-    r4 = Ranking(posicao = 4, autor = "Shin Koike")
-    r5 = Ranking(posicao = 5, autor = "Alex Atala")
-
     c1 = Comida(nome = "Lasanha", sabor = "4 queijos", 
     origem = "Itália", dificuldade_de_preparo = "Médio", 
-    nota = 9.5, ranking_id = 1)
+    nota = 9.5)
     c2 = Comida(nome = "Taco", sabor = "Carne ao molho", 
     origem = "México", dificuldade_de_preparo = "Médio", 
-    nota = 8.8, ranking_id = 2)
+    nota = 8.8)
     c3 = Comida(nome = "Torta", sabor = "Maçã", 
     origem = "EUA", dificuldade_de_preparo = "Fácil", 
-    nota = 9, ranking_id = 3)
+    nota = 9)
     c4 = Comida(nome = "Sushi", sabor = "Uramaki", 
     origem = "Japão", dificuldade_de_preparo = "Difícil", 
-    nota = 9, ranking_id = 4)
+    nota = 9)
     c5 = Comida(nome = "Churrasco", sabor = "Carne", 
     origem = "Brasil", dificuldade_de_preparo = "Difícil", 
-    nota = 9, ranking_id = 5)
+    nota = 9)
 
-    t1 = Classificacao(titulo = "melhor do mundo", categoria = "massa", comida_id = 1)
-    t2 = Classificacao(titulo = "melhor textura", categoria = "taco", comida_id = 2)
-    t3 = Classificacao(titulo = "melhor sobremesa", categoria = "torta", comida_id = 3)
-    t4 = Classificacao(titulo = "melhor sabor", categoria = "grãos", comida_id = 4)
-    t5 = Classificacao(titulo = "melhor prato", categoria = "cerne", comida_id = 5)
+    t1 = Classificacao(titulo = "melhor do mundo",  data_classificacao = "23/01/2018", categoria = "massa", comida = c1)
+    t2 = Classificacao(titulo = "melhor textura", data_classificacao = "12/07/2018", categoria = "taco", comida = c2)
+    t3 = Classificacao(titulo = "melhor sobremesa", data_classificacao = "16/05/2018", categoria = "torta", comida = c3)
+    t4 = Classificacao(titulo = "melhor sabor", data_classificacao = "11/10/2019", categoria = "grãos", comida = c4)
+    t5 = Classificacao(titulo = "melhor prato", data_classificacao = "23/12/2019", categoria = "cerne", comida = c5)
+
+    r1 = Ranking(posicao = 1, autor = "Massimo Bottura", comida = c1)
+    r2 = Ranking(posicao = 2, autor = "Jody Adams", comida = c3)
+    r3 = Ranking(posicao = 3, autor = "Alex Atala", comida = c5)
+
+    db.session.add(c1)
+    db.session.add(c3)
+    db.session.add(c5)
+
+    db.session.add(t1)
+    db.session.add(t3)
+    db.session.add(t5)
 
     db.session.add(r1)
     db.session.add(r2)
     db.session.add(r3)
-    db.session.add(r4)
-    db.session.add(r5)
-
-    db.session.add(c1)
-    db.session.add(c2)
-    db.session.add(c3)
-    db.session.add(c4)
-    db.session.add(c5)
-
-    db.session.add(t1)
-    db.session.add(t2)
-    db.session.add(t3)
-    db.session.add(t4)
-    db.session.add(t5)
 
     db.session.commit()
+
+    print(c1.json())
+    print(c3.json())
+    print(c5.json())
+
+    print(t1.json())
+    print(t3.json())
+    print(t5.json())
     
     print(r1.json())
     print(r2.json())
     print(r3.json())
-    print(r4.json())
-    print(r5.json())
 
-    print(c1.json())
-    print(c2.json())
-    print(c3.json())
-    print(c4.json())
-    print(c5.json())
-
-    print(t1.json())
-    print(t2.json())
-    print(t3.json())
-    print(t4.json())
-    print(t5.json())
